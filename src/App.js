82050f1f7f545c65ca26164ede9224e8d711ecfa
Aleksander Kuś <Kivlov84@users.noreply.github.com>
2020-09-16 16:52:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello World</h2>
         <p>
-          Welcome everybody to Buddy Webinar!!
+          Welcome absolutely everybody to Buddy Webinar!!
         </p>
       </header>
     </div>
